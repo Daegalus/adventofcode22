41cf87c83510219db459de9fc89e4455de52cff8
Yulian Kuncheff <yulian@kuncheff.com>
2022-12-02 23:50:02 -0800

    A bit more readable

--- src/day03.cr
+++ src/day03.cr
@@ -4,15 +4,17 @@ class Advent::Day03
 
         priority = ('a'..'z').zip(1..26).concat(('A'..'Z').zip(27..52)).to_h
         data = File.read("data/day03.txt").strip.split("\n")
-        
+
         total = data.sum do |line|
-            priority[(line.chars[..line.size//2] & line.chars[line.size//2..]).first]
+            intersection = (line.chars[..line.size//2] & line.chars[line.size//2..]).first
+            priority[intersection]
         end
 
         puts "  [Part 1] Total: #{total}"
 
         total2 = data.each_slice(3).sum do |lines|
-            priority[lines.map(&.chars).reduce { |a, b| a & b }.first]
+            intersection = lines.map(&.chars).reduce { |a, b| a & b }.first
+            priority[intersection]
         end
 
         puts "  [Part 2] Total: #{total2}"
