4687dcee2fd370e8e0e033eb8e4676cfd3e84c30
Yulian Kuncheff <yulian@kuncheff.com>
2022-12-05 00:37:33 -0800

    swith to module, use chars, use struct.

--- src/day05.cr
+++ src/day05.cr
@@ -1,13 +1,20 @@
-class Advent::Day05
+module Advent::Day05
+  struct Instruction
+    property qty, from, to
+  
+    def initialize(@qty : Int32, @from : Int32, @to : Int32)
+    end
+  end
+
   def self.part1
     puts "[Day 05] Camp Cleanup"
 
-    stacks, instructions = Advent::Day05.parse_input
+    stacks, instructions = parse_input
 
     instructions.each do |instruction|
       next unless instruction
-      stacks[instruction[:from]].pop(instruction[:qty]).reverse.each do |item|
-        stacks[instruction[:to]].push(item)
+      stacks[instruction.from].pop(instruction.qty).reverse.each do |item|
+        stacks[instruction.to].push(item)
       end
     end
 
@@ -16,12 +23,12 @@ class Advent::Day05
   end
 
   def self.part2
-    stacks, instructions = Advent::Day05.parse_input
+    stacks, instructions = parse_input
 
     instructions.each do |instruction|
       next unless instruction
-      stacks[instruction[:from]].pop(instruction[:qty]).each do |item|
-        stacks[instruction[:to]].push(item)
+      stacks[instruction.from].pop(instruction.qty).each do |item|
+        stacks[instruction.to].push(item)
       end
     end
 
@@ -35,7 +42,7 @@ class Advent::Day05
     start_state = start_state.lines[0..-2].map do |line|
       line.gsub("    ", "$")
         .gsub(/[\s\]\[]/, "")
-        .split("")
+        .chars
     end
 
     stacks = start_state.transpose.map do |s|
@@ -46,8 +53,7 @@ class Advent::Day05
 
     instructions = instructions.lines.map do |line|
       match = /move (?<qty>\d+) from (?<from>\d) to (?<to>\d)/.match(line)
-      next unless match  
-      { qty: match["qty"].to_i, from: match["from"].to_i - 1, to: match["to"].to_i - 1 }
+      Instruction.new(match["qty"].to_i, match["from"].to_i - 1, match["to"].to_i - 1) if match
     end
 
     {stacks, instructions}
